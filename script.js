420d92fd3d7dc38b9b2a24fd3d538c3d7bde141a
ROG_Chris <christian.lukowski96@gmail.com>
2024-11-14 19:30:11 +0100

    dd

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.getElementById('menu-toggle').addEventListener('click', function() {
   const verticalMenu = document.querySelector('.vertical-menu');
-  if (verticalMenu.style.display === 'block') {
+  verticalMenu.style.display = (verticalMenu.style.display === 'block') ? 'none' : 'block';
+});
+
+document.addEventListener('click', function(event) {
+  const target = event.target;
+  const verticalMenu = document.querySelector('.vertical-menu');
+  const menuToggle = document.getElementById('menu-toggle');
+  if (verticalMenu.style.display === 'block' && target !== verticalMenu && target !== menuToggle) {
       verticalMenu.style.display = 'none';
-  } else {
-      verticalMenu.style.display = 'block';
   }
 });
